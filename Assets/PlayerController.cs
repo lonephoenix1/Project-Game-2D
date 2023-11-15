6cccc1729e73e102aa7c630a46c9bd77547a27ad
lonephoenix1 <kgolec@edu.cdv.pl>
2023-11-15 11:33:52 +0100

    Movement_postaci(animacja)

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -9,20 +9,25 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private float stopForce = 10f;
     private Rigidbody rb;
     private Vector3 movementDirection;
+    private Animator anim;
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        anim = GetComponent<Animator>();
     }
 
     
     void Update()
     {
-        movementDirection = new Vector3(Input.GetAxis("Horizontal"), 0f, Input.GetAxis("Vertical"));
+        movementDirection = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0f);
 
         if (Input.GetButtonUp("Horizontal") || Input.GetButtonUp("Vertical"))
         {
             rb.velocity = Vector3.zero; 
         }
+
+        anim.SetBool("walk", movementDirection.x != 0);
+        anim.SetBool("walk_up", movementDirection.y > 0.1f);
     }
 
     private void FixedUpdate()
