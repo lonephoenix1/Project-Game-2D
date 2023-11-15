ed32c2ce95094ca8544fcee3bc87c9ed0f8b666d
DupciaBlada <wgarbowicz@edu.cdv.pl>
2023-11-15 10:10:45 +0100

    Pierdoły na mojej scenie

--- Assets/FollowCharacter.cs
+++ Assets/FollowCharacter.cs
@@ -4,14 +4,24 @@ public class FollowCharacter : MonoBehaviour
 {
     public Transform targetCharacter; // Referencja do postaci, za któr¹ pod¹¿amy
     public float moveSpeed = 3.0f;
+    public float detectionRadius = 5.0f; // Promieñ detekcji postaci
 
     void Update()
     {
         if (targetCharacter != null)
         {
-            Vector3 targetPosition = targetCharacter.position;
-            Vector3 moveDirection = (targetPosition - transform.position).normalized;
-            transform.position += moveDirection * moveSpeed * Time.deltaTime;
+            float distanceToTarget = Vector3.Distance(transform.position, targetCharacter.position);
+
+            // Sprawdzamy, czy postaæ jest w zasiêgu
+            if (distanceToTarget <= detectionRadius)
+            {
+                // Obliczamy kierunek ruchu
+                Vector3 targetPosition = targetCharacter.position;
+                Vector3 moveDirection = (targetPosition - transform.position).normalized;
+
+                // Ruch w kierunku postaci
+                transform.position += moveDirection * moveSpeed * Time.deltaTime;
+            }
         }
     }
 }
