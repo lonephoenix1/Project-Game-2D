262911d3d333cf00006ad72f7abba88797df5032
WiktorTward <wtwardowski@edu.cdv.pl>
2023-11-15 10:10:07 +0100

    2d

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -1,5 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEditor;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
@@ -7,6 +9,8 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private float movementSpeed = 5f;
     [SerializeField] private float MaxSpeed = 10f;
     [SerializeField] private float stopForce = 10f;
+    [SerializeField] GameObject prefab;
+
     private Rigidbody rb;
     private Vector3 movementDirection;
     void Start()
@@ -17,14 +21,22 @@ public class PlayerController : MonoBehaviour
     
     void Update()
     {
-        movementDirection = new Vector3(Input.GetAxis("Horizontal"), 0f, Input.GetAxis("Vertical"));
+        movementDirection = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0f);
 
         if (Input.GetButtonUp("Horizontal") || Input.GetButtonUp("Vertical"))
         {
             rb.velocity = Vector3.zero; 
         }
-    }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Shoot();
+        }
 
+    }
+    void Shoot() 
+    {
+        GameObject pocisk = Instantiate(prefab, transform.position, Quaternion.identity);
+    }
     private void FixedUpdate()
     {
 
