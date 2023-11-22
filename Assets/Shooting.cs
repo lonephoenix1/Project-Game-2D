b8f0d0741f1e48f8191aad18aea8123665e965d4
WiktorTward <wtwardowski@edu.cdv.pl>
2023-11-22 10:36:35 +0100

    strzelanie + chodzenie

--- Assets/Shooting.cs
+++ Assets/Shooting.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering;
 
 public class NewBehaviourScript : MonoBehaviour
 {
@@ -8,11 +9,16 @@ public class NewBehaviourScript : MonoBehaviour
     public GameObject bulletPrefab;
 
     public float bulletForce = 20f;
+    public float lifeTime = 3f;
     public Vector2 bulletDirection = Vector2.one;
-   
+
     void Update()
     {  
-        bulletDirection = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        if (Input.GetAxisRaw("Horizontal")!= 0 || Input.GetAxisRaw("Vertical") != 0) 
+        {
+            bulletDirection = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        }
+       
 
         if (Input.GetKeyDown(KeyCode.Space))
         {
@@ -24,5 +30,11 @@ public class NewBehaviourScript : MonoBehaviour
         GameObject bullet = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
         Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
         rb.AddForce(bulletDirection *  bulletForce ,ForceMode2D.Impulse);
+        StartCoroutine(SelfDestruct(bullet));
+    }
+    IEnumerator SelfDestruct(GameObject bullet)
+    {
+        yield return new WaitForSeconds(lifeTime);
+        Destroy(bullet);
     }
 }
